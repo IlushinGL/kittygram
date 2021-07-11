436164f7dd5cb14a612f6ca81e0ab351e365f48c
IlushinGL <ilushingl@yandex.ru>
2021-07-11 23:31:20 +0300

    Вьюсеты и роутер API

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path # , include
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
-from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-    path('cats/', CatList.as_view()),
-    path('cats/<int:pk>/', CatDetail.as_view()),
+    path('', include(router.urls)),
 ] 
